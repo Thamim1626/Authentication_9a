e998f84c5003325b44630bd31b6b7e6c017a897b
thamim <thamim1626>
2023-11-03 12:07:41 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
+
+const path = require("path");
+
+const dbPath = path.join(__dirname, "userData.db");
+
+const initializeServerAndDb = async () => {
+  try {
+    const db = await open({
+      fliename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(4000, () => {
+      console.log("server 4000 is starting .......");
+    });
+  } catch (e) {
+    process.exit(1);
+  }
+};
+initializeServerAndDb();
+
+//API 1
+app.get("/all/", (request, response) => {
+  response.send("hello");
+});
