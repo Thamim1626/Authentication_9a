77b6d0cd06198aaa626f27add2bfcafa769e3338
thamim ansari <thamimansari1626@gmail.com>
2023-11-05 23:04:18 +0000

    commit3

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ app.use(express.json());
 const sqlite3 = require("sqlite3");
 const { open } = require("sqlite");
 
+const bcrypt = require("bcrypt");
+
 const path = require("path");
 
 const dbPath = path.join(__dirname, "userData.db");
-
+let db = null;
 const initializeServerAndDb = async () => {
   try {
-    const db = await open({
+    db = await open({
       filename: dbPath,
       driver: sqlite3.Database,
     });
@@ -25,6 +27,31 @@ const initializeServerAndDb = async () => {
 initializeServerAndDb();
 
 //API 1
-app.get("/all/", (request, response) => {
-  response.send("hello");
+app.post("/register/", async (request, response) => {
+  const { username, name, password, gender, location } = request.body;
+  const hashPassword = await bcrypt.hash(password, 10);
+  let newUser = undefined;
+  const newUserQuery = `
+        select * 
+        from user
+        where username = ${username};`;
+  newUser = await db.get(newUserQuery);
+
+  if (newUser !== undefined) {
+    const registerInsertQuery = `
+    INSERT INTO
+    user (username,name, password ,gender, location)
+    values(
+        ${username}
+        ${name}
+        ${hashPassword}
+        ${gender}
+        ${location}
+    )`;
+    const registerInsert = await db.run(registerInsertQuery);
+    response.send("register successed");
+  } else {
+    response.status(400);
+    response.send("user Already exist");
+  }
 });
